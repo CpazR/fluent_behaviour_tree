810d506221faac28c84234980d88b319922efe17
Cpaz <nicky.shadow99@gmail.com>
2025-08-05 20:12:25 -0400

    added most remainting nodes

---
+++ addons/FluentBehaviourTree/BehaviourTree/Nodes/RepeatBehaviourNode.cs
@@ -0,0 +1,14 @@
+﻿using BehaviourTree.FluentBuilder;
+using Cpaz.FluentBehaviourTree.Nodes;
+using Godot;
+namespace fluent_behaviour_tree.addons.FluentBehaviourTree.BehaviourTree.Nodes;
+
+public partial class RepeatBehaviourNode : BehaviourNode {
+
+    [Export]
+    public required int repeatTimes = 5;
+
+    public override void BuildNode(FluentBuilder<GodotBehaviourContext> builder) {
+        builder.Repeat(Name, repeatTimes);
+    }
+}

562249f4891741b5a7378778915ba447eaf102de
Cpaz <nicky.shadow99@gmail.com>
2025-09-20 22:13:21 -0400

    feat: Added queue free "self" node

---
+++ addons/FluentBehaviourTree/BehaviourTree/Nodes/CommonActions/QueueFreeSelfBehaviourNode.cs
@@ -0,0 +1,16 @@
+﻿using BehaviourTree;
+using BehaviourTree.FluentBuilder;
+using Godot;
+namespace Cpaz.FluentBehaviourTree.Nodes.CommonActions;
+
+[GlobalClass]
+public partial class QueueFreeSelfBehaviourNode : ActionBehaviourNode {
+
+    public override void BuildNode(FluentBuilder<GodotBehaviourContext> builder) {
+        builder.Do(Name, context => {
+            context.owner.CallDeferred(Node.MethodName.QueueFree);
+            // Just keep running node indefinitely until queue free is called.
+            return BehaviourStatus.Running;
+        });
+    }
+}

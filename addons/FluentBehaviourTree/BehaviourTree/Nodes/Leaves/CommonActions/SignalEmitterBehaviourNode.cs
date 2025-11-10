80d1c7886a9b4bd5ab3fcbfee52518423b54a585
Cpaz <nicky.shadow99@gmail.com>
2025-11-09 22:02:05 -0500

    feat(common-actions): Signal emitter node

---
+++ addons/FluentBehaviourTree/BehaviourTree/Nodes/Leaves/CommonActions/SignalEmitterBehaviourNode.cs
@@ -0,0 +1,32 @@
+﻿using BehaviourTree;
+using BehaviourTree.FluentBuilder;
+using fluent_behaviour_tree.addons.FluentBehaviourTree.BehaviourTree.Nodes.Resources;
+using Godot;
+
+namespace fluent_behaviour_tree.addons.FluentBehaviourTree.BehaviourTree.Nodes.Leaves.CommonActions;
+
+/**
+ * From a given node, emit a signal
+ */
+[Tool]
+[GlobalClass]
+public partial class SignalEmitterBehaviourNode : ActionBehaviourNode {
+
+    private SignalExportResource _signalExportResource;
+
+    [Export]
+    public required SignalExportResource signalExportResource {
+        get => _signalExportResource;
+        set {
+            _signalExportResource = value;
+            _signalExportResource._toolOwningNode = this;
+        }
+    }
+
+    public override void BuildNode(FluentBuilder<GodotBehaviourContext> builder) {
+        builder.Do(Name, context => {
+            _signalExportResource.Emit(this);
+            return BehaviourStatus.Succeeded;
+        });
+    }
+}
---
+++ addons/FluentBehaviourTree/BehaviourTree/Nodes/Resources/SignalExportResource.cs
@@ -0,0 +1,89 @@
+﻿using Godot;
+using Godot.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace fluent_behaviour_tree.addons.FluentBehaviourTree.BehaviourTree.Nodes.Resources;
+
+[Tool]
+[GlobalClass]
+public partial class SignalExportResource : Resource {
+
+    private NodePath _parentNodeToSignal;
+
+    [Export]
+    public NodePath parentNodeToSignal {
+        get => _parentNodeToSignal;
+        set {
+            _parentNodeToSignal = value;
+            if (_toolOwningNode != null && parentNodeToSignal != null && parentNodeToSignal != "") {
+                var signalingNode = _toolOwningNode.GetNode(parentNodeToSignal);
+                var signals = signalingNode?.GetSignalList()
+                    .Select(dictionary => dictionary["name"].AsStringName());
+                signalList.Clear();
+                signalList.AddRange(signals?.ToList() ?? []);
+            }
+            NotifyPropertyListChanged();
+        }
+    }
+
+    private StringName signalName;
+
+    private readonly List<StringName> signalList = [];
+
+    public Node _toolOwningNode;
+
+    public void Emit(Node owningResourceNode) {
+        if (Engine.IsEditorHint()) {
+            return;
+        }
+
+        owningResourceNode.GetNode(parentNodeToSignal).EmitSignal(signalName);
+    }
+
+    public NodePath GetParentNodePath() {
+        return _parentNodeToSignal;
+    }
+
+    public StringName GetSignal() {
+        return signalName;
+    }
+
+    public override Array<Dictionary> _GetPropertyList() {
+        Array<Dictionary> propertyList = new Array<Dictionary>();
+
+        var propertySignalList = new Dictionary();
+        propertySignalList["name"] = "Signals";
+        propertySignalList["type"] = (int)Variant.Type.StringName;
+        propertySignalList["hint"] = (int)PropertyHint.Enum;
+        propertySignalList["hint_string"] = string.Join(",", signalList);
+        propertySignalList["usage"] = (long)PropertyUsageFlags.Storage | (long)PropertyUsageFlags.Editor;
+
+        propertyList.Add(propertySignalList);
+
+        return propertyList;
+    }
+
+    public override Variant _Get(StringName property) {
+
+        if (property == "Signals") {
+            if (_toolOwningNode != null && parentNodeToSignal != null && parentNodeToSignal != "") {
+                if (signalName == null) {
+                    signalName = signalList[0];
+                }
+                return Variant.From(signalName);
+            }
+        }
+
+        return default;
+    }
+
+    public override bool _Set(StringName property, Variant value) {
+
+        if (property == "Signals") {
+            signalName = value.ToString();
+            return true;
+        }
+
+        return false;
+    }
+}
